d7a7c18a550acb97c1086451c722c94832f30e97
teresalee1231 <69709178+teresalee1231@users.noreply.github.com>
2024-03-07 18:22:35 -0800

    wip starting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import About from "./components/About";
 import Cardholder from "./components/Cardholder";
+import Start from "./components/Start";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Start />
       <Header />
       <About />
       <Cardholder />
---
+++ src/components/Start.jsx
@@ -0,0 +1,13 @@
+function Start() {
+  return(
+    <div className="start">
+      <div className="copy">
+        <h1>Trols</h1>
+      </div>
+
+      <img src="images/trolls.jpg" alt="branch from trolls"/>
+    </div>
+  );
+}
+
+export default Start;
