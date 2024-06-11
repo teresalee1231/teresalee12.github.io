a6ef384b35d3b59c491dce414921369f784e20f1
teresalee1231 <69709178+teresalee1231@users.noreply.github.com>
2024-06-11 13:00:56 -0700

    fixed up hobbies page

--- src/components/Drawings.jsx
+++ src/components/Drawings.jsx
@@ -6,11 +6,28 @@ function Drawings () {
       </div>
 
       <div className="display">
+        <p class="display-ctn">
         <img src="images/ganyu.png" alt="Ganyu from Genshin Impact, drawn by me"/>
-        <img src="images/tony1.png" alt="Ganyu from Genshin Impact, drawn by me"/>
-        <img src="images/tony2.png" alt="Ganyu from Genshin Impact, drawn by me"/>
-        <img src="images/phone_wallpaper.png" alt="Ganyu from Genshin Impact, drawn by me"/>
-        <img src="images/phone_wallpaper2.png" alt="Ganyu from Genshin Impact, drawn by me"/>      </div>
+        </p>
+        <p class="display-ctn">
+        <img src="images/tony1.png" alt="Icon for my friend, drawn by me"/>
+        </p>
+        <p class="display-ctn">
+        <img src="images/tony2.png" alt="Icon for my friend, drawn by me"/>
+        </p>
+        <p class="display-ctn">
+        <img src="images/phone_wallpaper.png" alt="Current wallpaper, drawn by me"/>
+        </p>
+        <p class="display-ctn">
+        <img src="images/phone_wallpaper2.png" alt="Old wallpaper, drawn by me"/>
+        </p>
+
+
+
+
+
+
+        </div>
 
     </div>
   );
