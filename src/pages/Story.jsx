4e72a39dbbfbd6ca0eedb02bd90b06e2a31c53ac
teresalee1231 <69709178+teresalee1231@users.noreply.github.com>
2024-07-02 14:57:18 -0700

    hehe

--- src/pages/Story.jsx
+++ src/pages/Story.jsx
@@ -1,50 +1,58 @@
 function Stories () {
   return(
-    <>
-    <p>Humble beginnings</p>
-    <p>It all started when my mom signed me up for this after school program in
-      the third grade. It was out of the blue, and the more that I think about it,
-      I think it was just for me to stop watching TV after school (haha). I remember
-      being so nervous in this class, I didn't know anyone, and I didn't know what this
-      program was about. As it turns out, this program was a STEM program, and this was
-      my first introduction to robotics and programming.</p>
+    <div className="format">
 
-      <p>
-      As I was learning Scratch, I was amazed by the fact that I made this orange cat move across the screen. I realized, I could
-      make this cat do anything (within the bounds of Scratch). From then on, I was hooked on the idea of programming. Just the
-      idea of seeing the process of something being built, such as websites. That to me is very cool because it shows the hard work
-      and determination that is being put into things like this.
-      </p>
+      <div className="about">
+        <img src="/images/trolls.jpg"/>
+      <p>Humble beginnings</p>
+      <p>It all started when my mom signed me up for this after school program in
+        the third grade. It was out of the blue, and the more that I think about it,
+        I think it was just for me to stop watching TV after school (haha). I remember
+        being so nervous in this class, I didn't know anyone, and I didn't know what this
+        program was about. As it turns out, this program was a STEM program, and this was
+        my first introduction to robotics and programming.</p>
 
-      <p>
-      Though, that was the only instance of programming in my life up until high school.
-      </p>
+        <p>
+        As I was learning Scratch, I was amazed by the fact that I made this orange cat move across the screen. I realized, I could
+        make this cat do anything (within the bounds of Scratch). From then on, I was hooked on the idea of programming. Just the
+        idea of seeing the process of something being built, such as websites. That to me is very cool because it shows the hard work
+        and determination that is being put into things like this.
+        </p>
 
-      <p>
-      Growing up as a first generation, low income person, I never really had the
-      chance to explore and learn new things/opportunities.
-      As I mentioned in my word vomit up above,
-      I didn't know what programming was let alone computer science.
-      My parents didn't know what computer science was, my elementary and middle schools
-       didn't really have specific programs or exposure to programs like these.
-       (It was only during my last year of middle school that they introduced the
-       STEM program).
-      </p>
+        <p>
+        Though, that was the only instance of programming in my life up until high school.
+        </p>
 
-      <p>
-      What I'm trying to say is that I didn't really have the proper resources to
-      properly learn about computer science. (High school was a mess, if you are
-      interested to hear more, shoot me an email! I have a lot to talk about).
-      When I stepped foot into college, all my peers had some sort of connection
-      to programming. It was usually someone in their immediate family who are SWEs.
-      </p>
-      <p>
-      But that didn't stop me, I always remember why I want to do this.
-      I want to be able to create and design software/programs/technologies
-      to benefit communities and individuals. I want to be able to make technology
-      accessible to communities, such as the ones I grew up in.
-      </p>
-    </>
+        <p>
+        Growing up as a first generation, low income person, I never really had the
+        chance to explore and learn new things/opportunities.
+        As I mentioned in my word vomit up above,
+        I didn't know what programming was let alone computer science.
+        My parents didn't know what computer science was, my elementary and middle schools
+        didn't really have specific programs or exposure to programs like these.
+        (It was only during my last year of middle school that they introduced the
+        STEM program).
+        </p>
+
+        <p>
+        What I'm trying to say is that I didn't really have the proper resources to
+        properly learn about computer science. (High school was a mess, if you are
+        interested to hear more, shoot me an email! I have a lot to talk about).
+        When I stepped foot into college, all my peers had some sort of connection
+        to programming. It was usually someone in their immediate family who are SWEs.
+        </p>
+        <p>
+        But that didn't stop me, I always remember why I want to do this.
+        I want to be able to create and design software/programs/technologies
+        to benefit communities and individuals. I want to be able to make technology
+        accessible to communities, such as the ones I grew up in.
+        </p>
+      </div>
+
+
+    </div>
+
+
   );
 }
 
